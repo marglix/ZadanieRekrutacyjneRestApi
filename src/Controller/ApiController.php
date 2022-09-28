64e703cc9a5eb9985d998b388d7636e0319627ab
Martin Gladis <martin.gladis@outlook.com>
2022-09-28 19:51:44 +0200

    Init Project and add user registration

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class ApiController extends AbstractController
+{
+    private UserRepository $userRepository;
+
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/api/users", name="register", methods={"POST"})
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $parameters = json_decode($request->getContent(), true);
+        $username = $parameters['username'];
+        $password = $parameters['password'];
+        $user = $this->userRepository->findOneBy([
+            'username' => $username
+        ]);
+
+        if (!is_null($user)) {
+            return $this->json(['message' => 'User already exist.'], Response::HTTP_CONFLICT);
+        }
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $password)
+        );
+
+        $this->userRepository->add($user, true);
+
+        return $this->json([
+            "message" => "Registration completed successfully"
+        ], Response::HTTP_CREATED);
+    }
+}
